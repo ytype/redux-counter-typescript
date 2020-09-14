b5bfed60c770fb5f0d19f514997b75df7f784970
ytype <ytype33@gmail.com>
2020-09-14 14:13:35 +0900

    feat: redux counter

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import { createStore } from 'redux'
+import { state, action, initialState } from './types/index'
+
+const divToggle:HTMLElement = document.querySelector('.toggle')
+const counter:HTMLElement = document.querySelector('h1')
+const btnIncrease:HTMLElement = document.querySelector('#increase')
+const btnDecrease:HTMLElement = document.querySelector('#decrease')
+
+divToggle.onclick = () => {
+    store.dispatch(toggleSwitch())
+}
+btnIncrease.onclick = () => {
+    store.dispatch(increase(1))
+}
+btnDecrease.onclick = () => {
+    store.dispatch(decrease())
+}
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH'
+const INCREASE = 'INCREASE'
+const DECREASE = 'DECREASE'
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH })
+const increase = (difference:number) => ({ type: INCREASE, difference })
+const decrease = () => ({ type: DECREASE })
+
+const initialState:initialState = {
+    toggle: false,
+    counter: 0
+}
+
+function reducer(state:state = initialState, action: action) {
+    switch (action.type) {
+      case TOGGLE_SWITCH:
+        return {
+            ...state,
+            toggle: !state.toggle
+        }
+      case INCREASE:
+        return {
+            ...state,
+            counter: state.counter + action.difference
+        }
+      case DECREASE:
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+      default:
+        return state
+    }
+}
+
+const store = createStore(reducer)
+
+const render = () => {
+    const state = store.getState()
+    if (state.toggle) {
+      divToggle.classList.add('active')
+    } else {
+      divToggle.classList.remove('active')
+    }
+    counter.innerText = String(state.counter)
+  }
+
+const listener = () => {
+    console.log('상태가 업데이트됨')
+}
+
+render()
+store.subscribe(render)
+
+//const unsubscribe = store.subscribe(listener)
+//unsubscribe()
---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type state = {
+  toggle: boolean
+  counter: number
+}
+
+export type action = {
+    type: string
+    difference?: number
+}
+
+export type initialState = {
+    toggle: boolean,
+    counter: number
+}
